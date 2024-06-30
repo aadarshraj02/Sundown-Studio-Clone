e6d265e8cbc283cbb49cb6020cd921fd86781760
aadarshraj02 <aadarshraazz007@gmail.com>
2024-06-30 20:54:45 +0530

    added image effect on hover of each element

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+const elemContainer = document.querySelector("#elem-container");
+const imageContainer = document.querySelector("#image-container");
+
+elemContainer.addEventListener("mouseenter", () => {
+  imageContainer.style.display = "block";
+});
+elemContainer.addEventListener("mouseleave", () => {
+  imageContainer.style.display = "none";
+});
+
+var elemAll = document.querySelectorAll(".elem");
+elemAll.forEach((e) => {
+  e.addEventListener("mouseenter", () => {
+    var img = e.getAttribute("data-image");
+    imageContainer.style.backgroundImage = `url(${img})`;
+  });
+});
